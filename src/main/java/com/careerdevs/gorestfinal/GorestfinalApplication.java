d5c28203329d6d40136c0823bf353dd2fe169a63
mguadagni <guadagnimichael@gmail.com>
2022-04-19 19:36:25 -0400

    Initial commit

---
+++ src/main/java/com/careerdevs/gorestfinal/GorestfinalApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.gorestfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GorestfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GorestfinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/gorestfinal/GorestfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.gorestfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GorestfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
