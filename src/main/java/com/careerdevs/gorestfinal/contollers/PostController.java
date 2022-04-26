fdc579615b0307f11dcd4a43638a0bdc0d55f541
mguadagni <guadagnimichael@gmail.com>
2022-04-25 20:44:59 -0400

    Implemented upload all put request.

--- src/main/java/com/careerdevs/gorestfinal/contollers/PostController.java
+++ src/main/java/com/careerdevs/gorestfinal/contollers/PostController.java
@@ -6,10 +6,16 @@ import com.careerdevs.gorestfinal.utils.ApiErrorHandling;
 import com.careerdevs.gorestfinal.validation.PostValidation;
 import com.careerdevs.gorestfinal.validation.ValidationError;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
 
 @RestController
 @RequestMapping ("/api/posts")
@@ -17,11 +23,6 @@ public class PostController {
 
     @Autowired
     PostRepository postRepository;
-
-//    @GetMapping ("/test")
-//    public String testRoute () {
-//        return "Test";
-//    }
 
     @GetMapping ("/all")
     public ResponseEntity<?> getAllPosts () {
@@ -46,6 +47,94 @@ public class PostController {
         try {
 
             ValidationError errors = PostValidation.validatePost(newPost, postRepository, false);
+
+            if (errors.hasError()) {
+
+                throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, errors.toJSONString());
+
+            }
+
+            Post createdPost = postRepository.save(newPost);
+
+            return new ResponseEntity<> (createdPost, HttpStatus.CREATED);
+
+        } catch (HttpClientErrorException e) {
+
+            return ApiErrorHandling.customApiError(e.getMessage(), e.getStatusCode());
+
+        } catch (Exception e) {
+
+            return ApiErrorHandling.genericApiError(e);
+
+        }
+
+    }
+
+    @PostMapping ("/uploadall")
+    public ResponseEntity<?> uploadAll (
+            RestTemplate restTemplate
+    ){
+
+        try {
+
+            String url = "https://gorest.co.in/public/v2/posts";
+
+            ResponseEntity<Post[]> response = restTemplate.getForEntity(url, Post[].class);
+
+            Post[] firstPagePosts = response.getBody();
+
+            if (firstPagePosts == null) {
+
+                throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to get first page of posts");
+
+            }
+
+            ArrayList<Post> allPosts = new ArrayList<>(Arrays.asList(firstPagePosts));
+
+            HttpHeaders responseHeaders = response.getHeaders();
+
+            String totalPages = Objects.requireNonNull(responseHeaders.get("X-Pagination-Pages")).get(0);
+            int totalPgNum = Integer.parseInt(totalPages);
+
+            System.out.println(totalPgNum);
+
+            for (int i = 2; i <= totalPgNum; i++) {
+
+                String pageUrl = url + "?page=" + i;
+                Post[] pagePosts = restTemplate.getForObject(pageUrl, Post[].class);
+
+                if (pagePosts == null) {
+
+                    throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to get page " + i + " of posts");
+
+                }
+
+                allPosts.addAll(Arrays.asList(firstPagePosts));
+
+            }
+
+            postRepository.saveAll(allPosts);
+
+            return new ResponseEntity<>("Posts Created: " + allPosts.size(), HttpStatus.OK);
+
+        } catch(HttpClientErrorException e) {
+
+            return ApiErrorHandling.customApiError(e.getMessage(), e.getStatusCode());
+
+        } catch (Exception e) {
+
+            return ApiErrorHandling.genericApiError(e);
+
+        }
+
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<?> putPost (@RequestBody Post newPost) {
+
+        try {
+
+            ValidationError errors = PostValidation.validatePost(newPost, postRepository, true);
 
             if (errors.hasError()) {
 
--- src/main/java/com/careerdevs/gorestfinal/validation/ValidationError.java
+++ src/main/java/com/careerdevs/gorestfinal/validation/ValidationError.java
@@ -1,9 +1,12 @@
 package com.careerdevs.gorestfinal.validation;
 
+import com.careerdevs.gorestfinal.models.Post;
+import com.careerdevs.gorestfinal.repositories.PostRepository;
 import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class ValidationError {
 
@@ -29,6 +32,52 @@ public class ValidationError {
     public String toJSONString () {
 
         return new JSONObject(errors).toString();
+
+    }
+
+    public static ValidationError validatePost (Post post, PostRepository postRepo,
+                                                //UserRepository userRepo,
+                                                boolean isUpdate) {
+
+        ValidationError errors = new ValidationError();
+
+        if (isUpdate) {
+            if (post.getId() == 0) {
+                errors.addError("id", "ID can not be left blank");
+            } else {
+                Optional<Post> foundUser = postRepo.findById(post.getId());
+                if (foundUser.isEmpty()) {
+                    errors.addError("id", "No user found with the ID: " + post.getId());
+                }
+            }
+        }
+
+        String postTitle = post.getTitle();
+        String postBody = post.getBody();
+        long postUserId = post.getUser_id();
+
+        if (postTitle == null || postTitle.trim().equals("")) {
+            errors.addError("title", "Title can not be left blank");
+        }
+
+        if (postBody == null || postBody.trim().equals("")) {
+            errors.addError("body", "Body can not be left blank");
+        }
+
+        if (postUserId == 0) {
+            errors.addError("user_id", "User_ID can not be left blank");
+//        } else {
+//            // is the postUserId connected to an existing user.
+//            Optional<User> foundUser = userRepo.findById(postUserId);
+//
+//            if (foundUser.isEmpty()) {
+//                errors.addError("user_id", "User_ID is invalid because there is no user found with the id: " + postUserId);
+//            }
+//
+//
+        }
+
+        return errors;
 
     }
 
